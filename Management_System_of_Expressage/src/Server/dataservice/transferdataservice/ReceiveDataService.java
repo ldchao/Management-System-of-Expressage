55f8e2e77c00cb06f24f144bf6415a99e8b22ba3
ldchao <379905230@qq.com>
2015-10-25 23:45:47 +0800

    增加包和接口

---
+++ Management_System_of_Expressage/src/Server/dataservice/transferdataservice/ReceiveDataService.java
@@ -0,0 +1,11 @@
+package Server.dataservice.transferdataservice;
+
+import java.rmi.RemoteException;
+
+import Client.PO.ArriverorderPO;
+
+public interface ReceiveDataService {
+	
+	//在数据库中插入一个中转中心到达单的PO
+	public void insert(ArriverorderPO ap)throws RemoteException;
+}
