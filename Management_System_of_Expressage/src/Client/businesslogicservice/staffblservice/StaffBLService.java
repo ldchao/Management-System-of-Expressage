11a90053fcfa940b870badd75d20f543685113e0
FatBin <18813632558@163.c0m>
2015-10-26 20:48:40 +0800

    所有桩

--- Management_System_of_Expressage/src/Client/businesslogicservice/staffblservice/StaffBLService.java
+++ Management_System_of_Expressage/src/Client/businesslogicservice/staffblservice/StaffBLService.java
@@ -11,14 +11,15 @@ public interface StaffBLService {
 	public ArrayList<StaffVO> showStaff();
 	
 	//显示人员具体信息
-	public StaffPO checkStaff(String name);
+	public StaffVO checkStaff(String name);
 	
 	public void addStaff(String name,String address,String position,String phone,String cellphone);
 	
 	//查询是否存在这个人员信息，如果有，就返回
-	public StaffPO searchStaff(String name);
+	public StaffVO searchStaff(String name);
 	
-	public void editStaff(String name);
+	public void editStaff(String name,String address,String position,
+	String phone,String cellphone);
 	
 	public void deleteStaff(String name);
 	
