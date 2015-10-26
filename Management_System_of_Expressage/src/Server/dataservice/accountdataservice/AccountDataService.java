adbd9ffc7a51a1c72f60062fad08de382ad19152
EarthMonkey <794637366@qq.com>
2015-10-26 09:28:48 +0800

    增加Server的data

---
+++ Management_System_of_Expressage/src/Server/dataservice/accountdataservice/AccountDataService.java
@@ -0,0 +1,21 @@
+package Server.dataservice.accountdataservice;
+
+import java.util.ArrayList;
+
+import Client.PO.AccountPO;
+import Client.VO.AccountVO;
+
+public interface AccountDataService {
+
+	// 增加账户
+	public void insert(AccountPO po);
+
+	// 修改账户
+	public void update(AccountPO po);
+
+	// 根据账户名查找并删除账户
+	public void delete(String name);
+
+	// 根据关键字查找所有可能的账户
+	public ArrayList<AccountVO> find(String key);
+}
---
+++ Management_System_of_Expressage/src/Server/dataservice/billdataservice/BillDataService.java
@@ -0,0 +1,15 @@
+package Server.dataservice.billdataservice;
+
+import java.util.ArrayList;
+
+import Client.PO.StartinfoPO;
+import Client.VO.StartinfoVO;
+
+public interface BillDataService {
+
+	// 新建账
+	public void insert(StartinfoPO po);
+
+	// 查找所有帐
+	public ArrayList<StartinfoVO> find();
+}
