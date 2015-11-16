1965c873ba5b1891634093d404e59d290f3d434c
FatBin <18813632558@163.c0m>
2015-11-16 19:34:42 +0800

    a

--- Management_System_of_Expressage/src/Client/businesslogicservice/listinblservice/ReceiverinBLService.java
+++ Management_System_of_Expressage/src/Client/businesslogicservice/listinblservice/ReceiverinBLService.java
@@ -2,7 +2,7 @@ package Client.businesslogicservice.listinblservice;
 
 import State.ResultMessage;
 
-public interface ReceiverBLService {
+public interface ReceiverinBLService {
 	//新建收件人信息单
 	public ResultMessage addReceiver(String courier, String phoneOfcourier, String id,
 			String receiver, String address, String time, String cellphone);
