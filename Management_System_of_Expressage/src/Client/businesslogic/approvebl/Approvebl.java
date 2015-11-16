624a71725ef63233e5bb66e0df379465441f4e7a
FatBin <18813632558@163.c0m>
2015-11-16 21:18:10 +0800

    approve

---
+++ Management_System_of_Expressage/src/Client/businesslogic/approvebl/Approvebl.java
@@ -0,0 +1,33 @@
+package Client.businesslogic.approvebl;
+
+import java.util.ArrayList;
+
+import Client.businesslogicservice.approveblservice.ApproveBLService;
+
+public class Approvebl implements ApproveBLService{
+
+	@Override
+	public <T> ArrayList<T> showList(int state, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T checkList(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> void editList(T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endApprove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
