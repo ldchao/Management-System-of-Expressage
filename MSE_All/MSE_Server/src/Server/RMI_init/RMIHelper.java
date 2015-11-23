2539a78ee689b74eaeece1206f23bdcc74245fdb
ldchao <379905230@qq.com>
2015-11-23 21:23:25 +0800

    RMI补全

--- MSE_All/MSE_Server/src/Server/RMI_init/RMIHelper.java
+++ MSE_All/MSE_Server/src/Server/RMI_init/RMIHelper.java
@@ -19,7 +19,31 @@ public class RMIHelper {
     private static boolean inited = false;
 
     static {
-        NAMING_MAP.put("Warehouse_inData", Warehouse_inData.class);
+//        NAMING_MAP.put("AccountData", AccountData.class);
+//        NAMING_MAP.put("ApproveData", ApproveData.class);
+//        NAMING_MAP.put("BillData", BillData.class);
+////        NAMING_MAP.put("ListinData", ListinData.class);
+//        NAMING_MAP.put("CalculateData", CalculateData.class);
+//        NAMING_MAP.put("PayorderData", PayorderData.class);
+////        NAMING_MAP.put("ReceiFormData", ReceiFormData.class);
+//        NAMING_MAP.put("StatisticsData", StatisticsData.class);
+////        NAMING_MAP.put("ListinData", ListinData.class);
+//        NAMING_MAP.put("ReceiverData", ReceiverData.class);
+//        NAMING_MAP.put("LoadData", LoadData.class);
+//        NAMING_MAP.put("ShippingData", ShippingData.class);
+//        NAMING_MAP.put("ConstantData", ConstantData.class);
+//        NAMING_MAP.put("SalaryPolicyData", SalaryPolicyData.class);
+//        NAMING_MAP.put("StaffData", StaffData.class);
+//        NAMING_MAP.put("UserData", UserData.class);
+//        NAMING_MAP.put("Warehouse_inData", Warehouse_inData.class);
+//        NAMING_MAP.put("Warehouse_outData", Warehouse_outData.class);
+//        NAMING_MAP.put("Inventory_managementData", Inventory_managementData.class);
+////        NAMING_MAP.put("ReceiFormData", ReceiFormData.class);
+//        NAMING_MAP.put("ReceiveData", ReceiveData.class);
+//        NAMING_MAP.put("SendFormData", SendFormData.class);
+//        NAMING_MAP.put("TransferData", TransferData.class);
+//        NAMING_MAP.put("DriverData", DriverData.class);
+//        NAMING_MAP.put("VehicleData", VehicleData.class);
     }
 
     public synchronized static void init() throws ServerInitException {
