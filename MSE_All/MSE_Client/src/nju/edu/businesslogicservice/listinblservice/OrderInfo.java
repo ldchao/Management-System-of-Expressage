2d556a92891e78d307d4158207258aa67f2e7ec6
FatBin <18813632558@163.c0m>
2015-11-26 23:27:28 +0800

    新增运输中的订单存储空间

---
+++ MSE_All/MSE_Client/src/nju/edu/businesslogicservice/listinblservice/OrderInfo.java
@@ -0,0 +1,8 @@
+package nju.edu.businesslogicservice.listinblservice;
+/*
+ * author:Frebin
+ * function:update the message of the order that where the item is
+ */
+public interface OrderInfo {
+	public void getWeight(String id);
+}
---
+++ MSE_All/MSE_Client/src/nju/edu/businesslogicservice/listinblservice/UpdateInfo.java
@@ -0,0 +1,5 @@
+package nju.edu.businesslogicservice.listinblservice;
+
+public interface UpdateInfo {
+	public void update(String id,String message);
+}
