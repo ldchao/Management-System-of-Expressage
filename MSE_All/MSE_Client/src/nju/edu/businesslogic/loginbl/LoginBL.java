dceea06c802a6eab544ada266ca388ffac55119e
EarthMonkey <794637366@qq.com>
2015-11-30 11:17:17 +0800

    将记住用户名和密码保留在client

--- MSE_All/MSE_Client/src/nju/edu/businesslogic/loginbl/LoginBL.java
+++ MSE_All/MSE_Client/src/nju/edu/businesslogic/loginbl/LoginBL.java
@@ -14,7 +14,12 @@ import PO.LoginPO;
 import nju.edu.VO.UserVO;
 import nju.edu.businesslogic.staffbl.UserBL;
 import nju.edu.businesslogicservice.loginblservice.LoginBLService;
+import nju.edu.presentation.Business_hall_salesmanui.Business_hall_salesmanMainUI;
+import nju.edu.presentation.Transit_center_salesmanui.Transit_center_salesman_mainui;
+import nju.edu.presentation.Transit_center_storemasterui.Transit_center_storemaster_mainui;
+import nju.edu.presentation.courierui.Courier;
 import nju.edu.presentation.financial_staffui.fsframe;
+import nju.edu.presentation.general_managerui.Manager;
 import nju.edu.presentation.system_administratorui.Administratorframe;
 
 public class LoginBL implements LoginBLService {
@@ -53,16 +58,20 @@ public class LoginBL implements LoginBLService {
 
 		switch (limit) {
 		case "快递员":
-
+			Courier aCourier = new Courier();
+			aCourier.setVisible(true);
 			break;
 		case "营业厅业务员":
-
+			Business_hall_salesmanMainUI frame = new Business_hall_salesmanMainUI();
+			frame.setVisible(true);
 			break;
 		case "中转中心业务员":
-
+			Transit_center_salesman_mainui tcs = new Transit_center_salesman_mainui();
+			tcs.setVisible(true);
 			break;
 		case "总经理":
-
+			Manager mframe = new Manager();
+			mframe.setVisible(true);
 			break;
 		case "财务人员":
 			fsframe fs1 = new fsframe();
@@ -71,7 +80,8 @@ public class LoginBL implements LoginBLService {
 			fsframe fs2 = new fsframe();
 			break;
 		case "中转中心仓库管理员":
-
+			Transit_center_storemaster_mainui ts = new Transit_center_storemaster_mainui();
+			ts.setVisible(true);
 			break;
 		case "系统管理员":
 			Administratorframe ad = new Administratorframe();
@@ -81,8 +91,7 @@ public class LoginBL implements LoginBLService {
 
 	public void setUser(String user, String key) {
 		try {
-			FileWriter writer = new FileWriter(
-					new File("src/Remember.txt"));
+			FileWriter writer = new FileWriter(new File("src/Remember.txt"));
 			writer.write(user + ";" + key);
 			writer.close();
 		} catch (IOException e) {
