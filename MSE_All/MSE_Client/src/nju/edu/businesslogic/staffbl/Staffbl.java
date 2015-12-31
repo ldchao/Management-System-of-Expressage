ea8fa91b1515f61ecd4127d68ace4d5042e7c763
FatBin <18813632558@163.c0m>
2015-12-31 23:39:04 +0800

    出了点问题

--- MSE_All/MSE_Client/src/nju/edu/businesslogic/staffbl/Staffbl.java
+++ MSE_All/MSE_Client/src/nju/edu/businesslogic/staffbl/Staffbl.java
@@ -15,7 +15,6 @@ import nju.edu.dataservice.staffdataservice.StaffDataService;
 
 public class Staffbl implements StaffBLService,checkStaffInfo,editStaffInfo{
 	StaffDataService staffDataService=RMIHelper.getStaffData();
-	DeleteUserInterface DeleteUserInterface=new UserBL();
 	@Override
 	public ArrayList<StaffVO> showStaff() {
 		// TODO Auto-generated method stub
@@ -86,7 +85,6 @@ public class Staffbl implements StaffBLService,checkStaffInfo,editStaffInfo{
 		// TODO Auto-generated method stub
 		try {
 			staffDataService.delete(id);
-			DeleteUserInterface.deleteUser(id);
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
